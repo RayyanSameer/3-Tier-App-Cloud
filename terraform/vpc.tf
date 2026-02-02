12b828cb9d24d3a060903600610f748f7d6ac67e
Rayyan Sameer <turbocharged.tomato@gmail.com>
2026-02-02 11:29:32 +0530

    Fixed Versioning Issues in Infrastructure

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,7 +8,7 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   #define AZs
-  azs = ["ap-south-1a","ap-south-b"]
+  azs = ["ap-south-1a","ap-south-2b"]
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.101.0/24","10.0.102.0/24"]
   
